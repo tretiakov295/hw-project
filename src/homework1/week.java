6bdda29a26387c1d79ac96591f1de733a1fe034e
Your Name <you@example.com>
2021-03-02 21:21:49 +0300

    First commit

---
+++ src/homework1/week.java
@@ -0,0 +1,19 @@
+package homework1;
+
+public class week {
+    public static void main(String[] args) {
+        int num = 3;
+        switch (num) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("The week have 7 days");
+        }
+    }
+}
+
+
